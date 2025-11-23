3ac344133081c9259ccf7da45575b0979576134f
sebas2502 <81929841+sebas2502@users.noreply.github.com>
2025-11-23 17:15:43 -0300

    Delete backend directory

--- backend/src/config/jwtConfig.ts
+++
@@ -1,9 +0,0 @@
-// src/config/jwtConfig.ts
-
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const jwtConfig = {
-  secret: process.env.JWT_SECRET || "super_secreto_para_dev",
-  expiresIn: "8h", // duración del token
-};
